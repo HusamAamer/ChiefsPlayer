921fba2e47bb26132ee609fd1d1f3d9308cf3b2d
Husam Aamer <husam.a.alhatim@gmail.com>
2020-04-07 23:40:32 +0300

    Player Resolution Btn Disabled/Enabled colors

---
+++ ChiefsPlayer/Source/Controls Views/Over Player Controls/COPResolutionButton.swift
@@ -0,0 +1,22 @@
+//
+//  ResolutionButton.swift
+//  ChiefsPlayer
+//
+//  Created by Husam Aamer on 4/7/20.
+//  Copyright © 2020 AppChief. All rights reserved.
+//
+
+import UIKit
+
+class COPResolutionButton: UIButton {
+
+    override var isEnabled: Bool {
+        didSet {
+            if isEnabled {
+                backgroundColor = (backgroundColor ?? .white).withAlphaComponent(1)
+            } else {
+                backgroundColor = (backgroundColor ?? .white).withAlphaComponent(0.5)
+            }
+        }
+    }
+}
